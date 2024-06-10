097b3ccad265bf9c144a5af9790e6be612a68aa4
Sasha Mihalache <sasha.mihalache@gmail.com>
2024-06-10 16:15:25 +0300

    add prisma seed from csv

---
+++ prisma/seed.ts
@@ -0,0 +1,46 @@
+import { PrismaClient } from '@prisma/client';
+import { cities } from './static/cities';
+import { dishTypes } from './static/dishTypes';
+import { brands } from './static/brands';
+import { diets } from './static/diets';
+const prisma = new PrismaClient();
+
+async function seed() {
+  // Seed your database here
+  await prisma.city.createMany({
+    data: cities,
+  });
+
+  console.log('Cities seeded');
+
+  await prisma.dishType.createMany({
+    data: dishTypes,
+  });
+
+  console.log('Dish types seeded');
+
+  await prisma.brand.createMany({
+    data: brands,
+  });
+
+  console.log('Brands seeded');
+
+  await prisma.diet.createMany({
+    data: diets,
+  });
+
+  console.log('Diets seeded');
+}
+
+seed()
+  .then(() => {
+    console.log('Seed completed successfully 🌱');
+    process.exit(0);
+  })
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
