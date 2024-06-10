f8eec6c9b94f06abbbb859e68b59b27fa3331699
Sasha Mihalache <sasha.mihalache@gmail.com>
2024-06-10 15:55:23 +0300

    add prisma

---
+++ prisma/migrations/20240610125331_create_brands/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "Brand" (
+    "id" TEXT NOT NULL,
+    "name" VARCHAR(255) NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Brand_pkey" PRIMARY KEY ("id")
+);
---
+++ prisma/migrations/20240610125406_create_dish_type/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "DishType" (
+    "id" TEXT NOT NULL,
+    "name" VARCHAR(255) NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "DishType_pkey" PRIMARY KEY ("id")
+);
